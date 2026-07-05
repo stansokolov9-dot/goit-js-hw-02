b73c9a4ae92d1dc98557203fb55068ab3f0762ee
Stanislav <vasukovski@yahoo.com>
2026-07-05 18:47:43 +0100

    correct mistakes

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   if (totalPrice > customerCredits) {
     return "Insufficient funds!";
   }
-  return `'You ordered ${quantity} droids worth ${totalPrice} credits!'`;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
